42e31a0e7f6915371a4a5bca1c608454df2493db
Nick Tustison <ntustison@gmail.com>
2014-01-19 10:31:44 -0500

    Initial commit

---
+++ antsCorticalThicknessCommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DATA_DIR=${PWD}
+TEMPLATE_DIR=${DATA_DIR}/Template/
+OUT_DIR=${DATA_DIR}/OutputT1Only/
+export ANTSPATH=/Users/ntustison/Pkg/ANTs/bin/bin/
+
+bash ${ANTSPATH}antsCorticalThickness.sh -d 2 \
+  -a ${DATA_DIR}/IXI002-Guys-0828-T1_slice90.nii.gz \
+  -e ${TEMPLATE_DIR}template_slice80.nii.gz \
+  -m ${TEMPLATE_DIR}template_cerebrum_mask_slice80.nii.gz \
+  -p ${TEMPLATE_DIR}prior%d_slice80.nii.gz \
+  -o ${OUT_DIR}example
